70c16a1fec3144d0f084dda488b59daa226f72ac
tvigl <belberiinfo@gmail.com>
2026-01-26 17:16:50 +0200

    Create plugins-data.js

---
+++ plugins-data.js
@@ -0,0 +1,115 @@
+// Данные плагинов
+const plugins = [
+	{
+		name: "Плагин для добавления рейтингов: Imdb, Tmdb, MetaCritic, Rotten Tomato",
+		author: "No author",
+		version: "v1.0",
+		file: "plugins/rating_omdb.js",
+		description: "Плагин rating_omdb.js предназначен для расширения функциональности Lampa, добавляя комплексный рейтинг к фильмам и сериалам на основе данных из нескольких источников, а также отображая информацию о наградах и возрастных ограничениях.​",
+		badge: "Оформления",
+		image: "https://tvigl.github.io/plugins/img/rating_omdb.webp"
+	},
+	{
+		name: "Рейтинга на картах фильмов",
+		author: "No author", 
+		version: "v1.0",
+		file: "plugins/vote_colour.js",
+		description: "Плагин vote_colour.js — это пользовательское расширение для медиаплеера Lampa, которое изменяет цвет отображения рейтингов в зависимости от их числового значения, визуально подсвечивая качество контента.​",
+		badge: "Онлайн",
+		image: "https://youtu.be/QefvZaLiTAk?si=5vszGF-lZGA8iR6k"
+	},
+	{
+		name: "Подборок: Netflix, Prime, AppleTV, HBO, Hulu, Disney",
+		author: "Levande",
+		version: "v1.0.3",
+		file: "plugins/collections.js",
+		description: "Плагин collections.js добавляет в медиаплеер Lampa раздел «Підбірки» в настройках, позволяя пользователю управлять отображением категории с популярными стриминговыми платформами, такими как Netflix, Disney+, Prime Video, Apple TV+, HBO Max и Hulu.​",
+		badge: "Подборки",
+		image: "https://tvigl.github.io/plugins/img/collections.webp"
+	},
+	{
+		name: "Anime Skip",
+		author: "ipavlin98",
+		version: "v1.5.2",
+		file: "plugins/anime-skip.js",
+		description: "Пропуск опенингов и эндингов в аниме через базу Aniskip. Просмотреть или изменить информацию для конкретного аниме можно <a href='aniskip.html' target='_blank'>здесь.</a>",
+		badge: "Аниме",
+		image: "https://tvigl.github.io/plugins/img/anime-skip.jpg"
+	},
+	{
+		name: "Interface",
+		author: "ipavlin98",
+		version: "v3.0.1",
+		file: "plugins/int.js",
+		description: "Моя версия «стильного интерфейса» с множеством разных настроек. Для того чтобы всё выглядело и работало правильно, необходимо после установки плагина включить маленький размер интерфейса (Настройки > Интерфейс > Размер интерфейса > Меньше). P.S. Недавно в плагин была добавлена настройка для полного отключения Shorts.",
+		badge: "UI",
+		image: "https://tvigl.github.io/plugins/img/interface.jpg"
+	},
+	{
+		name: "Cardify Rating",
+		author: "ipavlin98",
+		version: "v1.3.0",
+		file: "plugins/cardify.js",
+		description: "Немного исправленная версия Cardify с парой фиксов и отображением рейтингов.",
+		badge: "UI",
+		image: "https://tvigl.github.io/plugins/img/cardify-rating.jpg"
+	},
+	{
+		name: "Logo",
+		author: "ipavlin98",
+		version: "v2.0.4",
+		file: "plugins/logo.js",
+		description: "Логотипы вместо названий. Плавная загрузка, кеширование, настройки разрешения.",
+		badge: "UI",
+		image: "https://tvigl.github.io/plugins/img/logo.jpg"
+	},
+	{
+		name: "Series Progress",
+		author: "ipavlin98",
+		version: "v1.1.2",
+		file: "plugins/series-progress-fix.js",
+		description: "Исправленное и более аккуратное отображение прогресса просмотра сериалов и фильмов на карточках.",
+		badge: "UI",
+		image: "https://tvigl.github.io/plugins/img/series-progress.jpg"
+	},
+	{
+		name: "Torrent Styles",
+		author: "ipavlin98",
+		version: "v1.0.8",
+		file: "plugins/torr-styles.js",
+		description: "Подсветка высоких сидов зелёным, высокого битрейта красным, зелёная рамка фокуса.",
+		badge: "UI",
+		image: "https://tvigl.github.io/plugins/img/torrent-styles.jpg"
+	},
+	{
+		name: "Series Skip",
+		author: "ipavlin98",
+		version: "v1.4.1",
+		file: "plugins/series-skip.js",
+		description: "Пропуск интро и аутро для сериалов. Работает для большинства популярных тайтлов.",
+		badge: "Плеер",
+		image: "https://tvigl.github.io/plugins/img/series-skip.jpg"
+	},
+	{
+		name: "Тестовый",
+		author: "1111",
+		version: "1",
+		date: "2025-01-19",
+		file: "https://levende.github.io/lampa-plugins/random-scheduled.js",
+		description: "Кратко",
+		badge: "Торренты",
+		image: "",
+		authorLink: "https://t.me/your_username",
+		discordLink: "https://discord.gg/your_invite_link",
+		githubLink: "https://github.com/your_username"
+	},
+	{
+		name: "Тестовый",
+		author: "Alexandr",
+		version: "1.0.0",
+		file: "https://tvigl.github.io/plugins/collections_12c77f7dbc312b67.js",
+		description: "Скоро",
+		badge: "Торренты",
+		image: "https://tvigl.github.io/plugins/img/4e42009cd310a3dc119c8f6bf0a377c6.png"
+	}
+];
