44d55956686f29f88e57d7020d3e452d99eb9071
tvigl <belberiinfo@gmail.com>
2026-02-13 20:32:03 +0200

    Update rating_year.js

--- rating_year.js
+++ rating_year.js
@@ -261,18 +261,6 @@
             document.head.appendChild(styleEl);
         }
 
-        var menuData = [
-            { title: 'Главная', action: 'main', icon: '<svg viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>' },
-            { title: 'Фильмы', action: 'movie', icon: '<svg viewBox="0 0 24 24"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/></svg>' },
-            { title: 'Сериалы', action: 'tv', icon: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"/></svg>' },
-            { title: 'Аниме', action: 'anime', icon: '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5-9h10v2H7z"/></svg>' },
-            { title: 'Каналы', action: 'channels', icon: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12zM5 7v2h2V7H5zm0 4v2h2v-2H5zm0 4v2h2v-2H5z"/></svg>' },
-            { title: 'Подборки', action: 'collections', icon: '<svg viewBox="0 0 24 24"><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z"/></svg>' },
-            { title: 'Избранное', action: 'favorite', icon: '<svg viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>' },
-            { title: 'История', action: 'history', icon: '<svg viewBox="0 0 24 24"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg>' },
-            { title: 'Настройки', action: 'settings', icon: '<svg viewBox="0 0 24 24"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.488.488 0 0 0-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54a.484.484 0 0 0-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32a.49.49 0 0 0-.12-.61l-2.03-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"/></svg>' }
-        ];
-
         var menuEl = document.createElement('div');
         menuEl.className = 'prisma-menu prisma-menu--hidden';
 
@@ -307,41 +295,108 @@
         listEl.className = 'prisma-menu__list';
 
         function renderMenuItems() {
+            if (!listEl) return;
+            
+            var oldHtml = listEl.innerHTML;
             listEl.innerHTML = '';
             
-            // Получаем динамические пункты от других плагинов, если они есть
-            var dynamicItems = [];
+            // Получаем все пункты меню от Lampa из всех возможных источников
+            var allLampaItems = [];
             if (window.Lampa && Lampa.Menu) {
-                // Пытаемся получить список через разные методы Lampa (в разных версиях по-разному)
-                var allItems = [];
-                if (typeof Lampa.Menu.get === 'function') allItems = Lampa.Menu.get();
-                else if (Lampa.Menu.items) allItems = Lampa.Menu.items;
+                if (typeof Lampa.Menu.get === 'function') {
+                    allLampaItems = Lampa.Menu.get();
+                } else if (Lampa.Menu.items) {
+                    if (Array.isArray(Lampa.Menu.items)) {
+                        allLampaItems = Lampa.Menu.items;
+                    } else if (typeof Lampa.Menu.items === 'object') {
+                        // Если это объект, пробуем извлечь значения
+                        allLampaItems = Object.values(Lampa.Menu.items);
+                    }
+                }
+                
+                // Проверяем наличие метода list()
+                if ((!allLampaItems || allLampaItems.length === 0) && typeof Lampa.Menu.list === 'function') {
+                    allLampaItems = Lampa.Menu.list();
+                }
+            }
 
-                console.log('Prisma Menu: All detected items:', allItems);
+            // На всякий случай убедимся, что это массив
+            if (!Array.isArray(allLampaItems)) allLampaItems = [];
 
-                // Фильтруем те, которых нет в нашем статичном списке
-                dynamicItems = allItems.filter(function(item) {
-                    var id = item.id || item.action;
-                    // Проверяем, нет ли уже такого ID в нашем базовом меню
-                    return !menuData.find(function(m) { return m.action === id; });
-                }).map(function(item) {
-                    // Обрабатываем перевод заголовка, если это ключ
-                    var title = item.title;
+            // Если список пуст, пробуем подождать еще немного
+            console.log('Prisma Menu: Detected items count:', allLampaItems.length);
+
+            // Наши базовые пункты
+            var baseMenu = [
+                { id: 'main', title: 'Главная', icon: '<svg viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>' },
+                { id: 'movie', title: 'Фильмы', icon: '<svg viewBox="0 0 24 24"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/></svg>' },
+                { id: 'tv', title: 'Сериалы', icon: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"/></svg>' },
+                { id: 'anime', title: 'Аниме', icon: '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5-9h10v2H7z"/></svg>' },
+                { id: 'channels', title: 'Каналы', icon: '<svg viewBox="0 0 24 24"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12zM5 7v2h2V7H5zm0 4v2h2v-2H5zm0 4v2h2v-2H5z"/></svg>' },
+                { id: 'collections', title: 'Подборки', icon: '<svg viewBox="0 0 24 24"><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z"/></svg>' },
+                { id: 'favorite', title: 'Избранное', icon: '<svg viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>' },
+                { id: 'history', title: 'История', icon: '<svg viewBox="0 0 24 24"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg>' },
+                { id: 'settings', title: 'Настройки', icon: '<svg viewBox="0 0 24 24"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.488.488 0 0 0-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54a.484.484 0 0 0-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32a.49.49 0 0 0-.12-.61l-2.03-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"/></svg>' }
+            ];
+
+            var finalItems = [];
+            var processedIds = {};
+
+            // 1. Сначала добавляем базовые пункты
+            baseMenu.forEach(function(baseItem) {
+                // Определяем возможные алиасы для поиска в Lampa
+                var aliases = [baseItem.id];
+                if (baseItem.id === 'main') aliases.push('full');
+                if (baseItem.id === 'movie') aliases.push('movies');
+                if (baseItem.id === 'collections') aliases.push('vitaliy');
+                
+                var lampaItem = allLampaItems.find(function(li) { 
+                    var liId = li.id || li.action || li.component;
+                    return aliases.indexOf(liId) !== -1;
+                });
+
+                if (lampaItem) {
+                    var title = lampaItem.title;
+                    if (window.Lampa.Lang && Lampa.Lang.translate && typeof title === 'string' && title.indexOf('{') === 0) {
+                        title = Lampa.Lang.translate(title);
+                    }
+                    finalItems.push({
+                        title: title || baseItem.title,
+                        action: lampaItem.id || lampaItem.action || lampaItem.component || baseItem.id,
+                        icon: lampaItem.icon || baseItem.icon
+                    });
+                    // Помечаем все алиасы как обработанные
+                    aliases.forEach(function(a) { processedIds[a] = true; });
+                    processedIds[lampaItem.id || lampaItem.action || lampaItem.component] = true;
+                } else {
+                    finalItems.push({
+                        title: baseItem.title,
+                        action: baseItem.id,
+                        icon: baseItem.icon
+                    });
+                    processedIds[baseItem.id] = true;
+                }
+            });
+
+            // 2. Добавляем все остальные пункты от плагинов (NUM, Studios и т.д.)
+            allLampaItems.forEach(function(lampaItem) {
+                var id = lampaItem.id || lampaItem.action || lampaItem.component;
+                if (id && !processedIds[id]) {
+                    var title = lampaItem.title;
                     if (window.Lampa.Lang && Lampa.Lang.translate) {
                         title = Lampa.Lang.translate(title);
                     }
-                    
-                    return {
-                        title: title,
-                        action: item.id || item.action,
-                        icon: item.icon || '<svg viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>'
-                    };
-                });
-            }
+                    finalItems.push({
+                        title: title || id,
+                        action: id,
+                        icon: lampaItem.icon || '<svg viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>'
+                    });
+                    processedIds[id] = true;
+                }
+            });
 
-            var fullMenu = menuData.concat(dynamicItems);
-
-            fullMenu.forEach(function(item) {
+            // Рендерим
+            finalItems.forEach(function(item) {
                 var li = document.createElement('li');
                 li.className = 'prisma-menu__item selector';
                 li.dataset.action = item.action;
@@ -368,7 +423,16 @@
 
                 listEl.appendChild(li);
             });
+
+            // Если контент изменился, уведомляем контроллер
+            if (listEl.innerHTML !== oldHtml && !menuEl.classList.contains('prisma-menu--hidden')) {
+                Lampa.Controller.collectionSet(menuEl);
+            }
         }
+
+        // Запускаем периодическую проверку в течение первых 15 секунд
+        var refreshInterval = setInterval(renderMenuItems, 2000);
+        setTimeout(function() { clearInterval(refreshInterval); }, 15000);
 
         // Перехватываем добавление новых пунктов меню
         if (window.Lampa && Lampa.Menu) {
@@ -453,7 +517,7 @@
                 }
 
                 var originalItem = allItems.find(function(item) {
-                    return (item.id || item.action) === action;
+                    return (item.id || item.action || item.component) === action;
                 });
 
                 if (originalItem && typeof originalItem.onSelect === 'function') {
